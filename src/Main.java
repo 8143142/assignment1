e2bcb0e3f0588caec30c7b2d4bdd29bb56064385
Nauriz <tasbula03@mail.ru>
2023-04-07 00:43:42 +0600

    Problem 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
+import java.util.Scanner;
+
 public class Main {
+    /*
+        @ findMin - definition of min
+        @ size - length of array
+        @ arr - array
+        @ return - returning minimum value
+    */
+    public static int findMin(int[] arr){
+        if(arr == null || arr.length == 0){
+            return -1;
+        }
+        int minVal = arr[0];
+        for(int i = 1; i< arr.length; i++){
+            if(arr[i] < minVal){
+                minVal = arr[i];
+            }
+        }
+        return minVal;
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        int size = scanner.nextInt();
+        int[] arr = new int[size];
+        for(int i=0; i<size; i++){
+            arr[i] = scanner.nextInt();
+        }
+        int minVal = findMin(arr);
+        System.out.println(minVal);
+        scanner.close();
     }
 }
